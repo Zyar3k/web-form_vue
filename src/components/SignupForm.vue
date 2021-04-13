b3e5c8cfec8c1210f68ebe2d842f125a0e4529d7
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-13 16:54:26 +0200

    Select input

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -4,9 +4,16 @@
     <input type="email" required v-model="email" />
     <label>Password:</label>
     <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -15,6 +22,7 @@ export default {
     return {
       email: "",
       password: "",
+      role: "designer",
     };
   },
 };
@@ -38,7 +46,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
